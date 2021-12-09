cb96043013f0335390fb9f1773220d5ffd9c9946
Jay Kittler <Jaykitt19@gmail.com>
2021-12-09 11:14:41 -0500

    Started Game Test

---
+++ game.js
@@ -0,0 +1,14 @@
+    v  ar moveDiff = 5;
+        right = true;
+        up = false;
+        tank;
+        angle = 0;
+        div;
+
+        tank = document.getElementById("player");
+        
+        function mouseMove(x){
+            if(cursor){
+                if(x.raw )
+            }
+        }
